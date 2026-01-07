5a1a85e200e17ba7f05d064c096959a83b72007d
bno229-max <bno229@gmail.com>
2026-01-07 17:45:54 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,16 +1,24 @@
-const CACHE_NAME = 'pode-parar-v1';
+const CACHE_NAME = 'pode-parar-v2';
 const ASSETS = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/icone-192.png',
-  '/icone-512.png'
+  '/livre-para-parar/',
+  '/livre-para-parar/index.html',
+  '/livre-para-parar/manifest.json',
+  '/livre-para-parar/icone-192.png',
+  '/livre-para-parar/icone-512.png'
 ];
 
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then((res) => {
+      return res || fetch(e.request);
+    })
+  );
 });
